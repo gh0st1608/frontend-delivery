7f8c5828b13ccc3aa2d57a9aa485c29de54315af
gh0st1608 <egalindoa@uni.pe>
2025-11-11 14:46:14 -0500

    init

---
+++ app/_layout.tsx
@@ -0,0 +1,26 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/use-color-scheme';
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="login" options={{ presentation: 'modal', title: 'Login' }}/>
+        <Stack.Screen name="register" options={{ presentation: 'modal', title: 'Register' }}/>
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
