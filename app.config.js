4aaa84a87d2799f29a41dd2f4828fa5cc2f04147
gh0st1608 <egalindoa@uni.pe>
2025-11-27 15:40:08 -0500

    feature<deploy> - add buildspec

--- app.config.js
+++ app.config.js
@@ -5,8 +5,14 @@ export default {
     name: "frontend-delivery",
     slug: "frontend-delivery",
     version: "1.0.0",
+    android: {
+      package: "com.solutionserj.foodstoresdc", // ← AGREGA ESTO
+    },
     extra: {
       API_URL: process.env.API_URL,
+      eas: {
+        projectId: "4d7c7d91-fd54-4df4-ae4b-87f2e705a458",
+      },
     },
   },
 };
